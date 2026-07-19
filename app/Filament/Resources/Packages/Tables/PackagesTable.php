bd5e6905aee6057f3556e5f88148df5611ab8d82
Fajar Ari <nugrahafajarari@gmail.com>
2026-07-19 19:21:05 +0700

    feat: tambah kolom indikator is_focus_topic di tabel admin Package

--- app/Filament/Resources/Packages/Tables/PackagesTable.php
+++ app/Filament/Resources/Packages/Tables/PackagesTable.php
@@ -7,6 +7,7 @@ use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ReplicateAction;
 use Filament\Actions\ViewAction;
+use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
@@ -28,6 +29,9 @@ class PackagesTable
                     ->searchable(),
                 TextColumn::make('type')
                     ->badge(),
+                IconColumn::make('is_focus_topic')
+                    ->label('Fokus Topik')
+                    ->boolean(),
                 TextColumn::make('price')
                     ->money('IDR')
                     ->sortable(),
